7d14915939ee46ee7e0933bc41a190dca61bf65a
wfrancelino <wellingtonfrancelino25@gmail.com>
2022-10-28 12:57:38 -0300

    Created .travis.yml file

---
+++ src/main/java/com/santoswellington/bookstoremanager/config/SwaggerConfig.java
@@ -0,0 +1,90 @@
+package com.santoswellington.bookstoremanager.config;
+
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.RequestHandlerProvider;
+import springfox.documentation.spring.web.WebMvcRequestHandler;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.DocumentationPluginsBootstrapper;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.spring.web.readers.operation.HandlerMethodResolver;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+import static springfox.documentation.RequestHandler.byPatternsCondition;
+import static springfox.documentation.spring.web.paths.Paths.ROOT;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.santoswellington.bookstoremanager";
+    private static final String API_TITLE = "Books Manager Course";
+    private static final String API_DESCRIPTION = "Primeiro projeto usando uma API RESTful e documentando com Swagger";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Wellington Santos";
+    private static final String CONTACT_GITHUB = "https://github.com/WellingtonF95";
+    private static final String CONTACT_EMAIL = "wellingtonfrancelino25@gmail.com";
+
+
+    @Bean
+    public InitializingBean removeSpringfoxHandlerProvider(DocumentationPluginsBootstrapper bootstrapper) {
+        return () -> bootstrapper.getHandlerProviders().removeIf(WebMvcRequestHandlerProvider.class::isInstance);
+    }
+    @Bean
+    public RequestHandlerProvider customRequestHandlerProvider(Optional<ServletContext> servletContext, HandlerMethodResolver methodResolver, List<RequestMappingInfoHandlerMapping> handlerMappings) {
+        String contextPath = servletContext.map(ServletContext::getContextPath).orElse(ROOT);
+        return () -> handlerMappings.stream()
+                .filter(mapping -> !mapping.getClass().getSimpleName().equals("IntegrationRequestMappingHandlerMapping"))
+                .map(mapping -> mapping.getHandlerMethods().entrySet())
+                .flatMap(Set::stream)
+                .map(entry -> new WebMvcRequestHandler(contextPath, methodResolver, tweakInfo(entry.getKey()), entry.getValue()))
+                .sorted(byPatternsCondition())
+                .collect(toList());
+    }
+    RequestMappingInfo tweakInfo(RequestMappingInfo info) {
+        if (info.getPathPatternsCondition() == null) return info;
+        String[] patterns = info.getPathPatternsCondition().getPatternValues().toArray(String[]::new);
+        return info.mutate().options(new RequestMappingInfo.BuilderConfiguration()).paths(patterns).build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+            .apiInfo(buildApiInfo());
+
+
+
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME,CONTACT_GITHUB,CONTACT_EMAIL))
+                .build();
+    }
+
+
+
+}
